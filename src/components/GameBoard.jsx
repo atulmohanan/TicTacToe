7ff9a72ac7b3fa7edf3ddff6f685a43ff7d6e53a
Atul Mohanan <107179770+atulmohanan@users.noreply.github.com>
2024-02-08 15:46:54 +0530

    Add files via upload

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+
+export default function GameBoard( {onSelectSquare,board} ) {
+ 
+ return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled = {playerSymbol !== null}>
+                    {playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
